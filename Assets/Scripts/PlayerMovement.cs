6d8122683ea3d09fafb93d8b456071e16510d682
sigridanita93 <79100365+sigridanita93@users.noreply.github.com>
2021-03-13 20:18:25 +0100

    Player Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,30 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
- 
+    [SerializeField] private float moveForce;
+    
+    private float moveDir; 
+    private Rigidbody2D myRB;
+
+    private void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        var moveAxis = Vector2.right * moveDir;
+
+        myRB.AddForce(moveAxis * moveForce, ForceMode2D.Force);
+    }
+
     public void Move(InputAction.CallbackContext context)
     {
-        print(message:context.ReadValue<float>());
+        moveDir = context.ReadValue<float>();
     }
 
     public void Jump(InputAction.CallbackContext context)
     {
-        print(message: context.ReadValue<float>());
+        print(message:context.ReadValue<float>());
     }
 }
